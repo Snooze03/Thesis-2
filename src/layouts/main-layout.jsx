e1e95c9144c81f3ff5254780317543a1d7bbad32
Snooze03 <baayjohnzeus@gmail.com>
2025-07-20 17:42:35 +0800

    Created a bottom navbar

--- src/layouts/main-layout.jsx
+++ src/layouts/main-layout.jsx
@@ -1,16 +1,20 @@
 import { cn } from "@/lib/utils"
+import { Navbar } from "@/components/navbar";
 
 // Layout for the different sections: profile, workouts, history, etc
 const MainLayout = ({ children }) => {
     return (
-        <div className={cn(
-            "mx-auto h-auto w-auto",
-            "xs:max-w-md p-4 lg:max-w-lg"
-        )}>
-            <div className="space-y-4 w-full h-full">
-                {children}
+        <>
+            <div className={cn(
+                "mx-auto h-auto w-auto",
+                "xs:max-w-md p-4 lg:max-w-lg"
+            )}>
+                <div className="space-y-4 w-full h-full">
+                    {children}
+                </div>
             </div>
-        </div>
+            <Navbar />
+        </>
     );
 }
 

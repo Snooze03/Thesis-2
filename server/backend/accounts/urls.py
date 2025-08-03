4d0cab9900235529f2b087ae8ec78579fe899a34
Snooze03 <baayjohnzeus@gmail.com>
2025-08-03 19:46:47 +0800

    edited urls

--- server/backend/accounts/urls.py
+++ server/backend/accounts/urls.py
@@ -4,9 +4,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("signup/", CreateAccountView.as_view(), name="signup"),
-    path("login/", TokenObtainPairView.as_view(), name="login-get_token"),
-    path("accounts/token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
+    path("token/", TokenObtainPairView.as_view(), name="login-get_token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
     # includes login & logout view
     # REMOVE LATER ON BECAUSE YOU HAVE A CUSTOM LOGIN VIEW
-    path("accounts-auth/", include("rest_framework.urls")),
+    # path("accounts-auth/", include("rest_framework.urls")),
 ]

1996a606801f9a670ad16537b3943fdd8eabdf26
Snooze03 <baayjohnzeus@gmail.com>
2025-08-03 11:45:03 +0800

    configured settings for backend email login

--- server/backend/accounts/backends.py
+++ server/backend/accounts/backends.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
 
+# Allows users to login via email instead of username
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         UserModel = get_user_model()

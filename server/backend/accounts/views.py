e295b3a8c4b419ef3e455daddb1bfa48c6d3b940
Snooze03 <baayjohnzeus@gmail.com>
2025-08-03 11:45:39 +0800

    created create user view

--- server/backend/accounts/views.py
+++ server/backend/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Account
+from rest_framework import generics
+from .serializers import AccountSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 # Create your views here.
+class CreateAccountView(generics.CreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [AllowAny]

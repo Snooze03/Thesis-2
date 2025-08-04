9e0089fb4b9126cd48caa9df497b6a4f1256cc53
Snooze03 <baayjohnzeus@gmail.com>
2025-08-04 22:27:59 +0800

    fixed profile responsiveness

--- client/src/pages/profile/profile-dashboard.jsx
+++ client/src/pages/profile/profile-dashboard.jsx
@@ -5,6 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 import api from "@/api";
 import { useNavigate } from "react-router-dom";
 import { clsx } from "clsx";
+import { cn } from "@/lib/utils";
 import { MainLayout } from "@/layouts/main-layout";
 import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
@@ -27,7 +28,7 @@ const Profile = () => {
             <SectionTitle>Profile</SectionTitle>
             <ProfileCard userName={userName} />
 
-            <SectionSubTitle>Progerss Report</SectionSubTitle>
+            <SectionSubTitle>Progress Report</SectionSubTitle>
             <ReportSettings />
 
             <SectionSubTitle>Generated Reports</SectionSubTitle>
@@ -50,7 +51,7 @@ const ProfileCard = ({ userName }) => {
     ]
 
     return (
-        <Card className="@container/profile gap-3 pt-0" >
+        <Card className="gap-3 pt-0" >
             <CardHeader className="flex justify-between items-center gap-3 py-4 bg-primary rounded-t-lg">
                 <div className="flex items-center gap-3">
                     <Avatar className="size-10">
@@ -66,35 +67,35 @@ const ProfileCard = ({ userName }) => {
                 <div className="grid grid-cols-3 items-center border-b-2 border-gray-300 pb-2 gap-3 md:gap-none">
                     {/* Stats */}
                     <div className="grid grid-rows-2 place-items-center">
-                        <p className="font-semibold text-lg @max-xs/profile:text-sm">
-                            <Dumbbell className="inline mr-1.5 stroke-green-500 size-4 @max-xs/profile:size-3 " />
+                        <p className="font-semibold text-lg max-xs:text-md">
+                            <Dumbbell className="inline mr-1.5 stroke-green-500 size-4 max-xs:size-4 " />
                             12
                         </p>
-                        <p className="text-gray-600 @max-xs/profile:text-sm">Workouts</p>
+                        <p className="text-gray-600 max-xs:text-sm">Workouts</p>
                     </div>
                     <div className="grid grid-rows-2 place-items-center">
-                        <p className="font-semibold text-lg @max-xs/profile:text-sm">
-                            <Flag className="inline mr-1.5 stroke-violet-500 size-4 @max-xs/profile:size-3" />
-                            55 kg
+                        <p className="font-semibold text-lg max-xs:text-md">
+                            <Flag className="inline mr-1.5 stroke-violet-500 size-4 max-xs:size-4" />
+                            55 <span className="text-gray-800 font-normal max-xs:text-xs">kg</span>
                         </p>
-                        <p className="text-gray-600 @max-xs/profile:text-sm">Weight</p>
+                        <p className="text-gray-600 max-xs:text-sm">Weight</p>
                     </div>
                     <div className="grid grid-rows-2 place-items-center ">
-                        <p className="font-semibold text-lg @max-xs/profile:text-sm">
-                            <Apple className="inline mr-1.5 stroke-red-400 size-4 @max-xs/profile:size-3" />
-                            -5 kg
+                        <p className="font-semibold text-lg max-xs:text-md">
+                            <Apple className="inline mr-1.5 stroke-red-400 size-4 max-xs:size-4" />
+                            -5 <span className="text-gray-800 font-normal max-xs:text-xs">kg</span>
                         </p>
-                        <p className="text-gray-600 @max-xs/profile:text-sm">Progress</p>
+                        <p className="text-gray-600 max-xs:text-sm">Progress</p>
                     </div>
                 </div>
 
                 {/* Streak Stat */}
                 <div className="flex justify-between gap-3 mt-4">
-                    <div className="@max-xs/profile:text-sm">
-                        <Target className="inline mr-3 stroke-orange-300 size-5 @max-xs/profile:size-4"></Target>
+                    <div className="max-xs:text-sm flex items-center ">
+                        <Target className="mr-3 stroke-orange-300 size-5 max-xs:size-4" />
                         Current Streak
                     </div>
-                    <Badge className="font-semibold @max-xs/profile:text-xs">7 days</Badge>
+                    <Badge className="font-semibold max-xs:text-xs">7 days</Badge>
                 </div>
             </CardContent>
         </Card >
@@ -106,17 +107,17 @@ const ReportSettings = () => {
     const [reportType, setReportType] = useState(isShort);
 
     return (
-        <Card className="@container/report">
+        <Card>
             <CardContent className="space-y-4">
                 {/* Days Inteval */}
                 <Label htmlFor="report-interval">Generate Report Every</Label>
-                <div className="grid grid-cols-3 gap-3 @max-xs/report:grid-cols-5 @max-2xs:grid-cols-6">
-                    <div className="relative w-full block col-span-2 @max-xs/report:col-span-3">
+                <div className="grid grid-cols-3 gap-3 max-xs:grid-cols-5 max-2xs:grid-cols-6">
+                    <div className="relative w-full block col-span-2 max-xs:col-span-3">
                         <Calendar className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 pointer-events-none" />
                         <Input id="report-interval" type="number" placeholder="22" className="pl-10" />
                     </div>
                     <Select>
-                        <SelectTrigger className="w-full @max-xs/report:col-span-2 @max-2xs/report:col-span-3">
+                        <SelectTrigger className="w-full max-xs:col-span-2 max-2xs:col-span-3">
                             <SelectValue placeholder="Days" />
                         </SelectTrigger>
                         <SelectContent>
@@ -129,13 +130,13 @@ const ReportSettings = () => {
 
                 {/* Time */}
                 <Label htmlFor="report-time-interval">Time</Label>
-                <div className="grid grid-cols-3 gap-3 @max-xs/report:grid-cols-5 @max-2xs/report:grid-cols-6">
-                    <div className="relative w-full block col-start-1 col-end-3 @max-xs/report:col-span-3">
+                <div className="grid grid-cols-3 gap-3 max-xs:grid-cols-5 max-2xs:grid-cols-6">
+                    <div className="relative w-full block col-start-1 col-end-3 max-xs:col-span-3">
                         <Clock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 pointer-events-none" />
                         <Input id="report-time-interval" type="number" placeholder="10:30" className="pl-10" />
                     </div>
                     <Select>
-                        <SelectTrigger className="w-full @max-xs/report:col-span-2 @max-2xs/report:col-span-3">
+                        <SelectTrigger className="w-full max-xs:col-span-2 max-2xs:col-span-3">
                             <SelectValue placeholder="A.M" />
                         </SelectTrigger>
                         <SelectContent>
@@ -156,18 +157,18 @@ const ReportSettings = () => {
                             { "border-1 border-primary bg-primary-500 hover:bg-primary-400": reportType })}>
                         <CardContent className="px-2.5 py-1 space-y-2">
                             <CardTitle className="font-normal">Short</CardTitle>
-                            <CardDescription className="text-start @max-sm/report:text-xs @max-xs/report:text-2xs">Brief overview of your progress</CardDescription>
+                            <CardDescription className="text-start max-xs:text-xs">Brief overview of your progress</CardDescription>
                         </CardContent>
                     </Card>
 
                     {/* sets report type to detailed */}
                     <Card onClick={() => setReportType(!isShort)}
                         className={clsx(
-                            "px-1 py-2 text-sm cursor-pointer hover:bg-gray-100 hover:shadow-lg transition delay-50 duration-200 ease-in-out @max-2xs:px-0",
+                            "px-1 py-2 text-sm cursor-pointer hover:bg-gray-100 hover:shadow-lg transition delay-50 duration-200 ease-in-out max-2xs:px-0",
                             { "border-1 border-primary bg-primary-500 hover:bg-primary-400": !reportType })}>
                         <CardContent className="px-2.5 py-1 space-y-2">
                             <CardTitle className="font-normal">Detailed</CardTitle>
-                            <CardDescription className="text-start @max-sm/report:text-xs @max-xs/report:text-2xs">In-depth analysis with personalized recommendations</CardDescription>
+                            <CardDescription className="text-start max-xs:text-xs">In-depth analysis with personalized feedback</CardDescription>
                         </CardContent>
                     </Card>
                 </div>
@@ -183,18 +184,22 @@ const ReportSettings = () => {
 
 const Report = ({ number, date, description }) => {
     return (
-        <Card className="@container/previous gap-2 hover:shadow-lg transition-shadow">
+        <Card className="gap-2 hover:shadow-lg transition-shadow">
             <CardHeader className="grid grid-cols-2 grid-rows-2 gap-1 content-center">
                 <CardTitle>Report #{number}</CardTitle>
                 <CardAction className="self-start">
-                    <button className="flex items-center gap-2 px-4 py-1 text-sm border-2 border-primary rounded-md text-primary font-semibold cursor-pointer hover:bg-primary hover:text-white transition delay-50 duration-200 ease-in-out @max-xs/previous:px-2">
-                        <Download strokeWidth="3" className="hover:stroke-white size-4 @max-xs/previous:size-3" />
+                    <button className={cn(
+                        "flex items-center gap-2 px-4 py-1 text-sm border-2 border-primary rounded-md text-primary font-semibold cursor-pointer",
+                        "hover:bg-primary hover:text-white transition delay-50 duration-200 ease-in-out ",
+                        "max-xs:px-2",
+                    )}>
+                        <Download strokeWidth="3" className="hover:stroke-white size-4" />
                         Export
                     </button>
                 </CardAction>
-                <CardDescription className="@max-xs/previous:text-xs">{date}</CardDescription>
+                <CardDescription className="max-xs:text-xs">{date}</CardDescription>
             </CardHeader>
-            <CardContent className="@max-xs/previous:text-xs">
+            <CardContent className="max-xs:text-sm">
                 <p>{description}</p>
             </CardContent>
         </Card>

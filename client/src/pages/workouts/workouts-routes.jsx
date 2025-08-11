bd437135b7bb0a159b3ca99769b077b57512dbdf
Snooze03 <baayjohnzeus@gmail.com>
2025-08-11 19:53:31 +0800

    added create template route

--- client/src/pages/workouts/workouts-routes.jsx
+++ client/src/pages/workouts/workouts-routes.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { WorkoutsDashboard } from "./workouts-dashboard";
+import { CreateTemplate } from "./workouts-search";
 
 export function WorkoutsRoutes() {
     return (
         <Routes>
             <Route index element={<WorkoutsDashboard />} />
-            <Route path="create" element={<h1>Create template</h1>} />
+            <Route path="create" element={<CreateTemplate />} />
         </Routes>
     );
 }

b603c73df0e1d75ad3b4611d2a5a37094a380441
Snooze03 <baayjohnzeus@gmail.com>
2025-08-12 19:38:16 +0800

    moved api-auth to main urls

--- server/backend/accounts/urls.py
+++ server/backend/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from .views import CreateAccountView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -6,7 +6,4 @@ urlpatterns = [
     path("signup/", CreateAccountView.as_view(), name="signup"),
     path("token/", TokenObtainPairView.as_view(), name="login-get_token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
-    # includes login & logout view
-    # REMOVE LATER ON BECAUSE YOU HAVE A CUSTOM LOGIN VIEW
-    # path("accounts-auth/", include("rest_framework.urls")),
 ]

b57451d225cf56b84624c59c8761b7718849187b
Snooze03 <baayjohnzeus@gmail.com>
2025-08-12 21:44:35 +0800

    added get request for templates

--- client/src/pages/workouts/workouts-dashboard.jsx
+++ client/src/pages/workouts/workouts-dashboard.jsx
@@ -1,13 +1,15 @@
 "use client"
 
+import api from "@/api";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import { MainLayout } from "@/layouts/main-layout";
 import { SectionTitle, SectionSubTitle, SectionSubText } from "@/components/ui/section-title";
 import { Accordion } from "@/components/ui/accordion";
 import { WorkoutTemplate } from "./workouts-template";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
-
-import { useNavigate } from "react-router-dom";
 
 
 const WorkoutsDashboard = () => {
@@ -26,6 +28,21 @@ const WorkoutsDashboard = () => {
 function Routines() {
     const navigate = useNavigate();
 
+    const getTemplates = async () => {
+        const response = await api.get("workouts/templates/");
+        console.log(response.data);
+        return response.data;
+    }
+
+    const { data, isPending, isError } = useQuery({
+        queryKey: ["templates"],
+        queryFn: getTemplates,
+    });
+
+    if (isPending) {
+        return <h1>Loading...</h1>
+    }
+
     return (
         <>
             <div className="flex justify-between items-center border-b-2 pb-3">
@@ -37,9 +54,15 @@ function Routines() {
             </div>
 
             <Accordion type="single" collapsible className="space-y-3">
-                <WorkoutTemplate id="1" title="Push Day" workouts="2" />
-                <WorkoutTemplate id="2" title="Pull Day" workouts="2" />
-                <WorkoutTemplate id="3" title="Leg Day" workouts="2" />
+                {data.map(item => {
+                    return (
+                        <WorkoutTemplate
+                            key={item.id}
+                            id={item.id}
+                            title={item.title}
+                        />
+                    );
+                })}
             </Accordion>
         </>
     );
@@ -57,8 +80,8 @@ function Alternatives() {
             </div>
 
             <Accordion type="single" collapsible className="space-y-3">
-                <WorkoutTemplate id="1" title="Push Day" workouts="2" />
-                <WorkoutTemplate id="2" title="Pull Day" workouts="2" />
+                {/* <WorkoutTemplate id="1" title="Push Day" workouts="2" />
+                <WorkoutTemplate id="2" title="Pull Day" workouts="2" /> */}
             </Accordion>
         </>
     );

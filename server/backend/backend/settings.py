c452d00b803859b31349ceff34529f9dea583b42
Snooze03 <baayjohnzeus@gmail.com>
2025-08-19 21:36:54 +0800

    added token rotate

--- server/backend/backend/settings.py
+++ server/backend/backend/settings.py
@@ -43,8 +43,9 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
 }
 
 # Application definition

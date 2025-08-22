9f01704d7baf81200e1c1053f47f17bec4b66c54
Snooze03 <baayjohnzeus@gmail.com>
2025-08-22 16:24:58 +0800

    added new routes

--- server/backend/accounts/urls.py
+++ server/backend/accounts/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path, include
-from .views import CreateAccountView, AccountProfileViewSet
+from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r"profile", AccountProfileViewSet, basename="account_profile")
+router.register(r"profile", views.AccountProfileViewSet, basename="account-profile")
+router.register(r"profiles", views.ProfileViewSet, basename="profile")
+router.register(
+    r"weight-history", views.WeightHistoryViewSet, basename="weight-history"
+)
 
 urlpatterns = [
-    path("signup/", CreateAccountView.as_view(), name="signup"),
+    path(
+        "signup/",
+        views.CombinedSignupView.as_view(),
+        name="signup-with-profile",
+    ),
     path("token/", TokenObtainPairView.as_view(), name="login-get_token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
     path("", include(router.urls)),
+    path("update/", views.AccountUpdateView.as_view(), name="account-update"),
 ]

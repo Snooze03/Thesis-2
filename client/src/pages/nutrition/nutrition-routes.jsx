2e572fca7d248a00a6f9620a45b1b39db6b4aace
Snooze03 <baayjohnzeus@gmail.com>
2025-09-21 14:52:21 +0800

    renamed route

--- client/src/pages/nutrition/nutrition-routes.jsx
+++ client/src/pages/nutrition/nutrition-routes.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { NutritionDashboard } from "./dashboard/nutrition-dashboard";
+import { AddFood } from "./add-food/nutrition-add";
 import { NotFound } from "@/routes";
 
 export function NutritionRoutes() {
     return (
         <Routes>
             <Route index element={<NutritionDashboard />} />
-            <Route path="" element={<h1>Nutrition Templates</h1>} />
+            <Route path="add" element={<AddFood />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     );

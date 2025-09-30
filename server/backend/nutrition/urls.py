8ce9fb2ba9782a800e29aad7b8cc448905c2ab76
Snooze03 <baayjohnzeus@gmail.com>
2025-09-30 17:30:20 +0800

    updated to use views in the views directory

--- server/backend/nutrition/urls.py
+++ server/backend/nutrition/urls.py
@@ -1,22 +1,30 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from . import views
+
+from .views import (
+    NutritionProfileViewSet,
+    FoodViewSet,
+    DailyEntryViewSet,
+    FoodEntryViewSet,
+    # Fatsecret
+    test_fatsecret_token,
+    search_foods,
+    get_food_details,
+)
 
 router = DefaultRouter()
 
 # Register ViewSets with the router
-router.register(
-    r"profiles", views.NutritionProfileViewSet, basename="nutrition-profile"
-)
-router.register(r"foods-db", views.FoodViewSet, basename="food")
-router.register(r"daily-entries", views.DailyEntryViewSet, basename="daily-entry")
-router.register(r"food-entries", views.FoodEntryViewSet, basename="food-entry")
+router.register(r"profiles", NutritionProfileViewSet, basename="nutrition-profile")
+router.register(r"foods-db", FoodViewSet, basename="food")
+router.register(r"daily-entries", DailyEntryViewSet, basename="daily-entry")
+router.register(r"food-entries", FoodEntryViewSet, basename="food-entry")
 
 urlpatterns = [
-    # FatSecret API endpoints
-    path("test-token/", views.test_fatsecret_token, name="test_fatsecret_token"),
-    path("foods/search/", views.search_foods, name="search_foods"),
-    path("foods/<int:food_id>/", views.get_food_details, name="food_details"),
-    # Include router URLs
+    # Fatsecret search and token test endpoints
+    path("test-token/", test_fatsecret_token, name="test-fatsecret"),
+    path("foods/search/", search_foods, name="search-foods"),
+    path("foods/<str:food_id>/", get_food_details, name="food-details"),
+    # Include the router URLs
     path("", include(router.urls)),
 ]

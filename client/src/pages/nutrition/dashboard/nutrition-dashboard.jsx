8e7b6a2b9abe101474ae03ff757afab9bcaa1733
Snooze03 <baayjohnzeus@gmail.com>
2025-10-01 11:20:58 +0800

    Used custom hook to display macro goals

--- client/src/pages/nutrition/dashboard/nutrition-dashboard.jsx
+++ client/src/pages/nutrition/dashboard/nutrition-dashboard.jsx
@@ -7,14 +7,25 @@ import { RadialProgress } from "@/components/ui/radial-progress";
 import { Progress } from "@/components/ui/progress";
 import { Coffee, Utensils, Moon, Cookie, Search, Zap, Wheat, Beef, Citrus, TrendingUp, Target } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useNutritionProfile } from "@/hooks/nutrition/useNutritionProfile";
 
 
 const NutritionDashboard = () => {
+    const {
+        profile,
+        isLoading,
+        isError,
+        error,
+        refetch
+    } = useNutritionProfile();
+
+    console.log("Nutrition Profile:", profile);
+
     return (
         <MainLayout>
             <SectionTitle>Nutrition</SectionTitle>
             <SectionSubText>Log and track your macros</SectionSubText>
-            <Macros />
+            <Macros userNutritionProfile={profile} />
 
             <SectionSubTitle>Add Food</SectionSubTitle>
             <AddFood />
@@ -23,7 +34,14 @@ const NutritionDashboard = () => {
 }
 
 
-const Macros = () => {
+const Macros = ({ userNutritionProfile }) => {
+
+    const daily_calories = userNutritionProfile ? userNutritionProfile.daily_calories_goal : 0;
+    const daily_carbs = userNutritionProfile ? userNutritionProfile.daily_carbs_goal : 0;
+    const daily_protein = userNutritionProfile ? userNutritionProfile.daily_protein_goal : 0;
+    const daily_fats = userNutritionProfile ? userNutritionProfile.daily_fat_goal : 0;
+
+
     return (
         <div className="flex justify-center w-full">
             <Carousel className="w-full sm:w-74">
@@ -34,7 +52,7 @@ const Macros = () => {
                                 <Zap className="inline mr-1.5 size-4" />
                                 Calories
                             </CardTitle>
-                            <RadialProgress value="1503" max="2000" size="xl" label="remaining" className="-mb-5" />
+                            <RadialProgress value="1000" max={daily_calories} size="xl" label="remaining" className="-mb-5" />
                             <div className="grid grid-cols-2 gap-3 text-sm p-4 -mb-2">
                                 <div className="grid auto-rows-auto gap-[4px] place-items-start bg-orange-100 px-4 py-3 rounded-lg">
                                     <p>
@@ -49,7 +67,7 @@ const Macros = () => {
                                         <Target className="inline size-4 mr-1" />
                                         Goal
                                     </p>
-                                    <p className="font-semibold">2000 <span className="text-muted-foreground font-normal">kcal</span></p>
+                                    <p className="font-semibold">{daily_calories} <span className="text-muted-foreground font-normal">kcal</span></p>
                                     <Progress value={100} className="bg-gray-100 [&>*]:bg-green-300 mt-1" />
                                 </div>
                             </div>
@@ -63,30 +81,30 @@ const Macros = () => {
                                     <Wheat className="size-4 m-2 stroke-white" />
                                 </div>
                                 <p className="col-start-2 text-md">Carbs</p>
-                                <p className="col-start-2 row-start-2 text-muted-foreground text-sm">120/160 g</p>
+                                <p className="col-start-2 row-start-2 text-muted-foreground text-sm">100/{daily_carbs} g</p>
                                 <p className="col-start-4 justify-self-end font-semibold text-md">40 g</p>
                                 <p className="col-start-4 justify-self-end text-muted-foreground text-sm">remaining</p>
-                                <Progress value={40} className="col-span-4 bg-gray-100 [&>*]:bg-orange-300" />
+                                <Progress value={100} max={daily_carbs} className="col-span-4 bg-gray-100 [&>*]:bg-orange-300" />
                             </div>
                             <div className="w-full px-6 grid grid-cols-[auto_1fr_1fr_1fr] grid-rows-3 items-center">
                                 <div className="mr-2 row-span-2 bg-green-300 rounded-full">
                                     <Beef className="size-4 m-2 stroke-white" />
                                 </div>
                                 <p className="col-start-2 text-md">Protein</p>
-                                <p className="col-start-2 row-start-2 text-muted-foreground text-sm">88/100 g</p>
+                                <p className="col-start-2 row-start-2 text-muted-foreground text-sm">88/{daily_protein} g</p>
                                 <p className="col-start-4 justify-self-end font-semibold text-md">80 g</p>
                                 <p className="col-start-4 justify-self-end text-muted-foreground text-sm">remaining</p>
-                                <Progress value={80} className="col-span-4 bg-gray-100 [&>*]:bg-green-300" />
+                                <Progress value={80} max={daily_protein} className="col-span-4 bg-gray-100 [&>*]:bg-green-300" />
                             </div>
                             <div className="w-full px-6 grid grid-cols-[auto_1fr_1fr_1fr] grid-rows-3 items-center">
                                 <div className="mr-2 row-span-2 bg-violet-300 rounded-full">
                                     <Citrus className="size-4 m-2 stroke-white" />
                                 </div>
                                 <p className="col-start-2 text-md">Fats</p>
-                                <p className="col-start-2 row-start-2 text-muted-foreground text-sm">50/90 g</p>
+                                <p className="col-start-2 row-start-2 text-muted-foreground text-sm">50/{daily_fats} g</p>
                                 <p className="col-start-4 justify-self-end font-semibold">20 g</p>
                                 <p className="col-start-4 justify-self-end text-muted-foreground text-sm">remaining</p>
-                                <Progress value={20} className="col-span-4 bg-gray-100 [&>*]:bg-violet-300" />
+                                <Progress value={20} max={daily_fats} className="col-span-4 bg-gray-100 [&>*]:bg-violet-300" />
                             </div>
                         </Card>
                     </CarouselItem>

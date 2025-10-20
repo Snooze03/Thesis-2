97d7afdca53631a9df42107d7963bb1dfcf20417
Snooze03 <baayjohnzeus@gmail.com>
2025-10-20 12:41:36 +0800

    updated urls to use the new packages

--- server/backend/accounts/urls.py
+++ server/backend/accounts/urls.py
@@ -1,23 +1,25 @@
 from django.urls import path, include
-from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
+from .views.account import AccountProfileViewSet, AccountUpdateView
+from .views.profile import ProfileViewSet
+from .views.weight_history import WeightHistoryViewSet
+from .views.signup import CombinedSignupView
+
 
 router = DefaultRouter()
-router.register(r"profile", views.AccountProfileViewSet, basename="account-profile")
-router.register(r"profiles", views.ProfileViewSet, basename="profile")
-router.register(
-    r"weight-history", views.WeightHistoryViewSet, basename="weight-history"
-)
+router.register(r"profile", AccountProfileViewSet, basename="account-profile")
+router.register(r"profiles", ProfileViewSet, basename="profile")
+router.register(r"weight-history", WeightHistoryViewSet, basename="weight-history")
 
 urlpatterns = [
     path(
         "signup/",
-        views.CombinedSignupView.as_view(),
+        CombinedSignupView.as_view(),
         name="signup-with-profile",
     ),
     path("token/", TokenObtainPairView.as_view(), name="login-get_token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
     path("", include(router.urls)),
-    path("update/", views.AccountUpdateView.as_view(), name="account-update"),
+    path("update/", AccountUpdateView.as_view(), name="account-update"),
 ]

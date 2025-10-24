d61aa797892075b137ddd310edef826aa8835a02
Snooze03 <baayjohnzeus@gmail.com>
2025-10-24 12:31:00 +0800

    hook to fetch a food from local database

--- client/src/hooks/nutrition/food/useFoodDatabase.js
+++ client/src/hooks/nutrition/food/useFoodDatabase.js
@@ -1,16 +1,18 @@
 import api from "@/api";
 import { useQuery, QueryClient } from "@tanstack/react-query";
 
-export function useFoodDatabase() {
+export function useFoodDatabase(foodDatabaseId) {
     const fetchFood = useQuery({
         queryKey: ['foodDatabase'],
-        queryFn: async ({ foodID }) => {
-            const response = await api.get(`/nutrition/food-database/${foodID}/`);
+        queryFn: async () => {
+            const response = await api.get(`/nutrition/foods-db/${foodDatabaseId}/`);
             return response.data;
         },
         onSuccess: (data) => {
             console.log('Fetched food database:', data);
-        }
+        },
+        enabled: !!foodDatabaseId, // Only run the query if foodDatabaseId is provided
+        retry: 5
     })
 
     return {

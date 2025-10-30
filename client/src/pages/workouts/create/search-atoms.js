56a7198151730731beb6f56f249447cc60d61b41
Snooze03 <baayjohnzeus@gmail.com>
2025-10-30 21:21:14 +0800

    merged the 2 atoms

--- client/src/pages/workouts/create/search-atoms.js
+++
@@ -1,6 +0,0 @@
-import { atom } from "jotai";
-
-export const searchTermAtom = atom("");
-export const selectedExercisesAtom = atom(new Map());
-
-export const exerciseConfigsAtom = atom(new Map());
--- client/src/pages/workouts/create/template-atoms.js
+++ client/src/pages/workouts/create/template-atoms.js
@@ -1,3 +1,12 @@
 import { atom } from "jotai";
 
+// Search Atoms
+export const searchTermAtom = atom("");
+export const selectedExercisesAtom = atom(new Map());
+
+// Template Atoms
 export const templateTitleAtom = atom("");
+export const exerciseConfigsAtom = atom(new Map());
+
+// Template Edit state
+export const isEditingTemplateAtom = atom(false);

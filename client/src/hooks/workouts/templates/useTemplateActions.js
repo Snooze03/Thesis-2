511238371151abcaa1891dc24fa984f7111d630e
Snooze03 <baayjohnzeus@gmail.com>
2025-11-02 12:24:58 +0800

    query to fetch a users workout history

--- client/src/hooks/workouts/templates/useTemplateActions.js
+++ client/src/hooks/workouts/templates/useTemplateActions.js
@@ -1,5 +1,6 @@
 import api from "@/api";
 import { useQuery, useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 /**
  * Hook for saving a completed workout and fetching workout history
@@ -11,15 +12,38 @@ export function useTemplateActions() {
             return response.data;
         },
         onSuccess: () => {
-            console.log("Template saved successfully");
+            toast.success("Workout Completed!")
         },
         onError: (error) => {
             console.error("Error saving template:", error);
         }
     })
 
+    const fetchWorkoutHistory = useQuery({
+        queryKey: ["workoutHistory"],
+        queryFn: async () => {
+            const response = await api.get("workouts/history/");
+            return response.data;
+        },
+        // onSuccess: (data) => {
+        //     console.log("Fetched workout history:");
+        // },
+        onError: (error) => {
+            console.log(`Error fetching workouts history: ${error}`);
+        },
+        staleTime: 5 * 60 * 1000, // 5 minutes
+        cacheTime: 30 * 60 * 1000, // 30 minutes
+    })
+
     return {
+        // For saving a completed workout template
         saveTemplate: saveTemplate.mutate,
+        // State
         isSaving: saveTemplate.isPending,
+
+        // Fetching workout history
+        workoutHistory: fetchWorkoutHistory.data,
+        // state
+        isFetchingHistory: fetchWorkoutHistory.isLoading,
     }
 }

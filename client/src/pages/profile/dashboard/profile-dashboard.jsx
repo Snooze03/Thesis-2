d4fcffbd5e439fffcf6b7ecd1404d8c5f92f17e0
Snooze03 <baayjohnzeus@gmail.com>
2025-11-18 13:46:17 +0800

    listed out a user's progress report

--- client/src/pages/profile/dashboard/profile-dashboard.jsx
+++ client/src/pages/profile/dashboard/profile-dashboard.jsx
@@ -1,5 +1,6 @@
 import { useScrollLock } from "@/hooks/useScrollLock";
 import { useAccountProfile } from "@/hooks/profile/useAccountProfile";
+import { fetchProgressReport } from "@/hooks/assistant/useProgressReport";
 import { MainLayout } from "@/layouts/main-layout";
 import { ProfileCard } from "./profile-card";
 import { WeightManager } from "./weight-chart";
@@ -15,11 +16,15 @@ const Profile = () => {
         isPending,
     } = useAccountProfile();
 
-    // console.log(account_profile);
+    const {
+        progressReports,
+        isLoading,
+        isError
+    } = fetchProgressReport();
 
-    useScrollLock(isPending);
+    useScrollLock(isPending || isLoading);
 
-    if (isPending) {
+    if (isPending || isLoading) {
         return (
             <MainLayout>
                 <Skeleton className="h-10 w-25 rounded-lg" />
@@ -42,9 +47,16 @@ const Profile = () => {
             <BeforeAndAfterPicture />
 
             <SectionSubTitle>Generated Reports</SectionSubTitle>
-            {/* <EmptyItems title="No progress reports yet." description="make sure to properly configure the settings" /> */}
-            {/* To Loop on using back-end later */}
-            <ReportCard number="1" date="March 12, 2025" description="Great progress on your strength goals! Your bench press has improved by 10% and you've been consistent with your workouts." />
+            {!progressReports || progressReports.length === 0 ? (
+                <EmptyItems title="No progress reports yet." description="make sure to properly configure the settings" />
+            ) : (
+                progressReports.map((report, index) => (
+                    <ReportCard
+                        key={index}
+                        data={report}
+                    />
+                ))
+            )};
         </MainLayout >
     );
 }

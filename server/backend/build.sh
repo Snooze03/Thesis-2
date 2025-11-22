9b0e472c60efa535f6df17463b75b4637feb95f0
Snooze <54021075+Snooze03@users.noreply.github.com>
2025-11-22 22:13:48 +0800

    Revert "Dev"

--- server/backend/build.sh
+++ server/backend/build.sh
@@ -5,7 +5,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-# Celery
-python manage.py migrate django_celery_beat
-python manage.py migrate django_celery_results

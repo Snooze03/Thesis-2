d2f7bbc96cbd955e83565b28b9f2feeb40b6560c
Snooze03 <baayjohnzeus@gmail.com>
2025-11-23 15:34:11 +0800

    added no input parameter

--- server/backend/build.sh
+++ server/backend/build.sh
@@ -4,8 +4,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py migrate
+python manage.py migrate --no-input
 
 # Celery
-python manage.py migrate django_celery_beat
-python manage.py migrate django_celery_results
+python manage.py migrate django_celery_beat --no-input
+python manage.py migrate django_celery_results --no-input

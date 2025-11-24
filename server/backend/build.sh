a9778b3a3aa6a4a5dd5f2b5a65ee0fd540743fbd
Snooze03 <baayjohnzeus@gmail.com>
2025-11-24 16:31:05 +0800

    fixed syntax error

--- server/backend/build.sh
+++ server/backend/build.sh
@@ -6,7 +6,7 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate --no-input
 
-if [[$CREATE_SUPERUSER]]; 
+if [[ $CREATE_SUPERUSER ]]; 
 then
     python manage.py createsuperuser --no-input --email $DJANGO_SUPERUSER_EMAIL --username $DJANGO_SUPERUSER_USERNAME --password $DJANGO_SUPERUSER_PASSWORD
     echo "Superuser created."

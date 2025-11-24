782918f210ee406f798c4beb4bf8ea91c47d60eb
Snooze03 <baayjohnzeus@gmail.com>
2025-11-24 13:47:20 +0800

    fixed condition

--- server/backend/build.sh
+++ server/backend/build.sh
@@ -6,7 +6,7 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate --no-input
 
-if [[$CREATE_SUPERUSER == "true"]]; 
+if [[$CREATE_SUPERUSER]]; 
 then
     python manage.py createsuperuser --no-input 
 fi

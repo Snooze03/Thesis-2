6831b6dfc1931ab1cb3c48959c3254c8471e5413
Snooze03 <baayjohnzeus@gmail.com>
2025-11-24 13:44:25 +0800

    added admin creation

--- server/backend/build.sh
+++ server/backend/build.sh
@@ -6,6 +6,11 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate --no-input
 
+if [[$CREATE_SUPERUSER == "true"]]; 
+then
+    python manage.py createsuperuser --no-input 
+fi
+
 # Celery
 python manage.py migrate django_celery_beat --no-input
 python manage.py migrate django_celery_results --no-input
